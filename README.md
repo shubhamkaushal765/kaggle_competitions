0fac643d60bd6d7f1b357050c11b75e2f9a9d056
shubhamkaushal <shubhamkaushal765@gmail.com>
2024-04-27 16:04:08 +0530

    Add audio feature extraction. TODO: add more, and save results.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Resources
 
-Audio Feature Extraction
+### Audio Feature Extraction
 
 - [Music Feature Extraction in Python](https://towardsdatascience.com/extract-features-of-music-75a3f9bc265d)
 - [Audio Feature Extraction](https://devopedia.org/audio-feature-extraction#:~:text=Audio%20feature%20extraction%20is%20a,is%20a%20representation%20of%20sound.)
@@ -13,3 +13,11 @@ Audio Feature Extraction
 - [pyAudioAnalysis](https://github.com/tyiannak/pyAudioAnalysis)
 - [AudioClassification - PyTorch](https://github.com/yeyupiaoling/AudioClassification-Pytorch)
 - [towhee](https://github.com/towhee-io/towhee)
+
+
+## TODO
+
+- Generate all the other metrics for all audios.
+- Add zero-crossing analysis to data_exploration.Data.audio_features - [source](https://www.analyticsvidhya.com/blog/2022/01/analysis-of-zero-crossing-rates-of-different-music-genre-tracks/)
+- Create different yet simmilar analysis to zero-crossing. For example, zero-max-mean line crossing. rms line crossing.
+- [speechprocessingbook.aalto.fi](https://speechprocessingbook.aalto.fi/Representations/Zero-crossing_rate.html#:~:text=To%20calculate%20of%20the%20zero,length%20signal%20you%20need%20operations.)
--- data_exploration.py
+++ data_exploration.py
@@ -1,3 +1,8 @@
+import os
+import librosa
+import sklearn
+
+import numpy as np
 import polars as pl
 import matplotlib.pyplot as plt
 
@@ -7,6 +12,7 @@ class Data:
         self, csv_path="data/birdclef2024/train_metadata.csv", plot_hist=False
     ):
         self.df = pl.read_csv(csv_path)
+        self.df = self.df[["primary_label", "rating", "url", "filename"]]
 
         label_col = "primary_label"
         unique_labels = sorted(list(set(self.df[label_col])))
@@ -41,7 +47,74 @@ class Data:
         print(f"Number of min values: {n_mins}")
         print(f"Number of max values: {n_maxs}")
 
+    def audio_features(self, data_root="./data/birdclef2024/train_audio/"):
+        """https://towardsdatascience.com/extract-features-of-music-75a3f9bc265d"""
+
+        row = self.df[0]
+        audio_path = os.path.join(data_root, row["filename"][0])
+
+        x, sr = librosa.load(audio_path, sr=None)
+
+        # Short Term Fourier Transform
+        X = np.array(librosa.stft(x))
+        Xdb = np.array(librosa.amplitude_to_db(abs(X)))
+        print(f"Short Term Fourier Transform: {X.shape}")
+        print(f"STFT in db: {Xdb.shape}")
+        print("################################")
+        # plt.figure(figsize=(14, 5))
+        # librosa.display.specshow(Xdb, sr=sr, x_axis="time", y_axis="hz")  # y_axis='log'
+        # plt.colorbar()
+        # plt.show()
+        ################################################################
+
+        # Zero Crossing Rate
+        zero_crossings = librosa.zero_crossings(x, pad=True)
+        print(f"Zero Crossings Shape: {zero_crossings.shape}")
+        print(f"Non-negative places of zero crossing: {sum(zero_crossings)}")
+        print(zero_crossings)
+        print("################################")
+        ################################################################
+
+        # Spectral Centroid
+        spectral_centroids = librosa.feature.spectral_centroid(y=x, sr=sr)[0]
+        print(f"Spectral Centroids Shape: {spectral_centroids.shape}")
+        print("################################")
+
+        # Computing the time variable for visualization
+        frames = range(len(spectral_centroids))
+        t = librosa.frames_to_time(frames, sr=sr)
+
+        # # Normalising the spectral centroid for visualisation
+        # def normalize(x, axis=0):
+        #     return sklearn.preprocessing.minmax_scale(x, axis=axis)
+
+        # # Plotting the Spectral Centroid along the waveform
+        # # librosa.display.waveplot(x, sr=sr, alpha=0.4)
+        # plt.plot(t, spectral_centroids, color="r")
+        # plt.show()
+        ################################################################
+
+        # Spectral Rolloff
+        spectral_rolloff = librosa.feature.spectral_rolloff(y=x, sr=sr)[0]
+        print(f"Spectral Rolloff Shape: {spectral_rolloff.shape}")
+        print("################################")
+
+        # librosa.display.waveshow(y=x, sr=sr)
+        # plt.plot(t, spectral_rolloff, color="r")
+        # plt.show()
+        ################################################################
+
+        # Mel-Frequency Cepstral Coefficients
+        mfccs = librosa.feature.mfcc(y=x, sr=sr)
+        print(f"Mel-Frequency Cepstral Coefficients: {mfccs.shape}")
+        print("################################")
+        # # Displaying  the MFCCs:
+        # librosa.display.specshow(mfccs, sr=sr, x_axis="time")
+        # plt.show()
+        ################################################################
+
 
 if __name__ == "__main__":
     ds = Data()
-    ds.generate_analytics()
+    # ds.generate_analytics()
+    ds.audio_features()
